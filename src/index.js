8b163939e831c1ac95fbb0deeb82c93346fd1b2a
Pre22 <pre.chika.22@gmail.com>
2020-09-17 15:11:51 +0100

    first react demo app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+
+let model ={ clicks: 0 };
+
+function render() {
+  ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App clicks={model.clicks} onClick={()=> { model.clicks += 1; render(); }} />
   </React.StrictMode>,
   document.getElementById('root')
 );
+  }
+render();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
